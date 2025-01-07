67de0d20f434e7f50cc6ba107601cd19e533a17b
Ahmed Khaled <130765437+Ahmed-Khaled-Abdelmaksod@users.noreply.github.com>
2025-01-07 17:35:53 +0200

    Create file

---
+++ 1408-StringMatchinginanArray/1408-StringMatchinginanArray.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def stringMatching(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        soln = []
+        for i in range(0,len(words)):
+
+            for j in range(0,len(words)):
+                if i == j:
+                    continue
+                if len(words[i]) <= len(words[j]):
+                   if words[i] in words[j]:
+                        soln.append(words[i])
+
+        return list(set(soln))
+
+
+
+        
